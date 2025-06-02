e4cf12f7f422b4b519b16e6eafef94574666295b
tuliobaruk <tuliobarukms@gmail.com>
2025-06-01 21:33:33 -0300

    feat: Remoção de templates e adicição de HomePage

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// Páginas Públicas
+import HomePage from "@/pages/HomePage";
+import NotFoundPage from "@/pages/NotFoundPage";
+
+
+const router = createBrowserRouter([
+	// Rotas Públicas
+	{
+		path: "/",
+		element: <HomePage />,
+		errorElement: <NotFoundPage />,
+	}
+]);
+
+export default router;
