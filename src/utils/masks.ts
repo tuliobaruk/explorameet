3ec62f104c5b4af068f6aa53149d9752b151e526
tuliobaruk <tuliobarukms@gmail.com>
2025-06-12 20:38:39 -0300

    feat(utils): Adiciona as funções utilizadas como máscara no formulário de registro

---
+++ src/utils/masks.ts
@@ -0,0 +1,33 @@
+export function maskPhone(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/^(\d{2})(\d)/, "($1) $2")
+		.replace(/(\d{5})(\d)/, "$1-$2")
+		.replace(/(-\d{4})\d+?$/, "$1");
+}
+
+export function maskCPF(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+}
+
+export function maskCNPJ(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/^(\d{2})(\d)/, "$1.$2")
+		.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+		.replace(/\.(\d{3})(\d)/, ".$1/$2")
+		.replace(/(\d{4})(\d)/, "$1-$2")
+		.replace(/(-\d{2})\d+?$/, "$1");
+}
+
+export function maskCpfCnpj(value: string) {
+	const digits = value.replace(/\D/g, "");
+	if (digits.length <= 11) {
+		return maskCPF(value);
+	}
+	return maskCNPJ(value);
+}
