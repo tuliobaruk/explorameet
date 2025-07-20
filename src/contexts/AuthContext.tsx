9f740284815862399c1b7a18c7426d8c6be037c4
Thiago Lins <thiago.jorge2001@gmail.com>
2025-07-19 22:58:59 -0300

    fix(AuthContext): remove a dependência useEffect para evitar renderização desnecessária

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -176,7 +176,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
 	useEffect(() => {
 		checkAuthStatus();
-	}, [checkAuthStatus]);
+	}, []);
 
 	const value: AuthContextType = {
 		user,
