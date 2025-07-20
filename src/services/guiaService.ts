463df99072f827efa6fa367ff01ffd7aa1ac9156
Thiago Lins <thiago.jorge2001@gmail.com>
2025-07-20 00:55:23 -0300

    feat(GuiaService): adiciona serviço para obter guia por ID

---
+++ src/services/guiaService.ts
@@ -0,0 +1,8 @@
+import { apiClient } from "./authService";
+
+export class GuiaService {
+  static async getGuiaById(guiaId: string) {
+    const response = await apiClient.get(`/guias/${guiaId}`);
+    return response.data;
+  }
+}
